7d18339adbcd0cb9a4036ed839043703070dbf86
Thomas LOEB <thomas.loeb@sogen.io>
2024-06-13 23:42:11 +0200

    replace with net8.0

--- src/SoundCloud.Api/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-[assembly: InternalsVisibleTo("SoundCloud.Api.Test")]
-[assembly: InternalsVisibleTo("SoundCloud.Api.IntegrationTest")]
--- test/SoundCloud.Api.Test/coverage.ps1
+++
@@ -1,2 +0,0 @@
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-dotnet msbuild /t:Coverage
